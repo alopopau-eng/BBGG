d858761e43c8ef32738e845e481fbb5a983bdee3
alopopau-eng <alopopau@gmail.com>
2025-12-31 13:23:11 +0300

    SAC

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,13 @@ import { getDatabase } from 'firebase/database';
 import { doc, getFirestore, setDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJOaDkXvm5XP3-Fu0LsuKvQ0_dZK5uf-k",
-  authDomain: "school-77936.firebaseapp.com",
-  databaseURL: "https://school-77936-default-rtdb.firebaseio.com",
-  projectId: "school-77936",
-  storageBucket: "school-77936.firebasestorage.app",
-  messagingSenderId: "783345000886",
-  appId: "1:783345000886:web:585c7579926bcb8f0ca117",
-  measurementId: "G-XX9L099J8H"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
